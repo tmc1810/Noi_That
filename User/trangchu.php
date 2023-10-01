4ae782903118c4e173c535cb2d2abc5facbbf6cc
Trần Mạnh Cường <128331877+tmc1810@users.noreply.github.com>
2023-10-01 17:14:41 +0700

    he

---
+++ User/trangchu.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="vn">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"/>
+    <link rel="stylesheet" href="./bootstrap5/css/bootstrap.min.css">
+    <link rel="shortcut icon" type="image/png" href="img/logo.png" />
+    <link rel="stylesheet" href="style.css">
+    <title>TMC INTERIOR</title>
+</head>
+<body>
+    <header class="navbar navbar-expand-lg bg-body-tertiary" id="header">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="./img/logo.png" alt="Logo" width="120" height="80" class="d-inline-block align-text-top">
+            </a>
+            
+            <ul class="nav justify-content-center">
+                <li class="nav-item"><a class="nav-link" href="trangchu.php">TRANG CHỦ</a></li>
+                <li class="nav-item"><a class="nav-link" href="sanpham.php">SẢN PHẨM</a></li>
+                <li class="nav-item"><a class="nav-link" href="gioithieu.php">GIỚI THIỆU</a></li>
+                <li class="nav-item"><a class="nav-link" href="lienhe.php">LIÊN HỆ</a></li>
+            </ul>
+
+            <ul class="nav justify-content-end">
+                <li><a class="fa-solid fa-magnifying-glass" href="#"></a></li>
+                <li><a class="fa-solid fa-cart-shopping" href="giohang.php"></a></li>
+                <li><a class="fa-regular fa-user" href="thongtintaikhoan.php"></a></li>
+            </ul>
+          </div>
+        </div>
+    </header>
+    
+    <div class="body-web">
+        <ul>
+            <li>
+                <div class="title">
+                    <p class="tmc">TMC </p> 
+                    <p> I N T E R I O R</p> 
+                </div>
+                <img src="./img/background1.jpg" width="100%">
+            </li>
+            <li>
+                <div class="main-gioithieu">
+                    <div class="main-title">
+                        <div>
+                            <h3>
+                                <span>01</span>
+                                <span class="hello">Giới thiệu</span>
+                                <hr>
+                            </h3>
+                            <h6>
+                                WHO WE ARE
+                                <hr>
+                            </h6>
+                        </div>
+                        <div class="comment">
+                            <h2>Công ty TNHH Nội Thất TMC Interior là doanh nghiệp chuyên sản xuất, kinh doanh các sản phẩm nội thất bằng gỗ. Kể từ khi thành lập đến nay, cùng với những nỗ lực không ngừng trong quá trình sản xuất kinh doanh, Luôn đặt lợi ích của khách hàng làm tiêu chí hoạt động. Công ty đã tạo cho mình được chỗ đứng vững chắc, được khách hàng tín nhiệm. Cùng với đội ngũ nhân viên giỏi về chuyên môn, năng động trong công việc. TMC Interior hứa hẹn sẽ mang đến sự hài lòng cho quý khách.</h2>
+                        </div>
+
+                        <button class="btn btn-warning"><a class="click" href="#">Tìm hiểu thêm</a></button>
+
+                        <div class="menu-list">
+                            <ul class="nav justify-content-center">
+                                <li class="nav-item">
+                                    <h1>10</h1>
+                                    <h7 style="color:#FFF;">Hơn 10 năm kinh</h7><br>
+                                    &emsp;<h7 style="color:#FFF;">nghiệm trong</h7><br>
+                                    &emsp;&emsp;&ensp;<h7 style="color:#FFF;">ngành</h7>
+                                </li>
+
+                                </li>
+                                <li class="nav-item">
+                                    <h1>20</h1>
+                                    <h7 style="color:#FFF;">Hơn 10 năm kinh</h7><br>
+                                    &emsp;<h7 style="color:#FFF;">nghiệm trong</h7><br>
+                                    &emsp;&emsp;&ensp;<h7 style="color:#FFF;">ngành</h7>
+                                </li>
+
+                                </li>
+                                <li class="nav-item">
+                                    <h1 style="margin-left: 2px;">88%</h1>
+                                    <h7 style="color:#FFF;">Hơn 10 năm kinh</h7><br>
+                                    &emsp;<h7 style="color:#FFF;">nghiệm trong</h7><br>
+                                    &emsp;&emsp;&ensp;<h7 style="color:#FFF;">ngành</h7>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="img-title">
+                            <img src="./img/hehe.jpg" width="450px" height="550px">
+                        </div>
+                    </div>
+                </div>
+                   <img src="./img/background2.jpg" width="100%" height="1000px"></li>
+            <li>
+                <div class="view"></div>
+                <img src="./img/background3.png" width="100%" height="1000px"></li>
+        </ul>
+        
+    </div>
+    <footer>
+            <div class="container-fluid">
+                <div class="list">
+                    <ul class="nav justify-content-lg-center">
+                        <ul class="nav flex-column">
+                            <li class="nav-item"><h5>SẢN PHẨM</h5></li>
+                            <li class="nav-item"><a href="#">Khóa thông minh</a></li>
+                            <li class="nav-item"><a href="#">Bàn làm việc</a></li>
+                            <li class="nav-item"><a href="#">Giường ngủ</a></li>
+                            <li class="nav-item"><a href="#">Tủ quần áo</a></li>
+                            <li class="nav-item"><a href="#">Sofa</a></li>
+                        </ul>
+
+                        <ul class="nav flex-column">
+                            <li class="nav-item"><h5>TRỢ GIÚP</h5></li>
+                            <li class="nav-item"><a href="#">Hướng dẫn mua hàng</a></li>
+                            <li class="nav-item"><a href="#">Hướng dẫn thanh toán</a></li>
+                            <li class="nav-item"><a href="#">Hướng dẫn đổi trả</a></li>
+                            <li class="nav-item"><a href="#">Chính sách giao hàng</a></li>
+                        </ul>
+
+                        <ul class="nav flex-column">
+                            <li><h5>VỀ CHÚNG TÔI</h5></li>
+                            <li><a href="#">Giới thiệu</a></li>
+                            <li><a href="#">Tin tức</a></li>
+                        </ul>
+
+                        <ul class="nav flex-column">
+                            <li class="nav-item"><h5>FANPAGE</h5></li>
+                            <li class="nav-item"><img src="./img/fanpage.png" width="100%"></li>
+                        </ul>
+
+                        <ul class="nav flex-column">
+                            <li class="nav-item"><h5>THEO DÕI CHÚNG TÔI</h5></li>
+                            <li class="nav-item"><img src="./img/follow.png" width="100%"></li>
+                        </ul>
+                    </ul>
+                </div><br>
+                <hr>
+                <center><h6 style="color: #FFF;">@Copy right 2023, Designed by Lanweb</h6></center>
+            </div>
+    </footer>
+    <script src="style.js"></script>
+</body>
+</html>
